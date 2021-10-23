722eb8ce037296c0acdc33f06aeac744fa7a3630
soyaibur <ovisoyaiburrahman@gmail.com>
2021-10-23 18:38:59 +0600

    full-round Branch

--- style.js
+++ style.js
@@ -1,5 +1,9 @@
 let btn = document.querySelector(".btn-theme-toggle")
+let sunMoonBox = document.querySelector(".sun-moon-container")
 
 btn.addEventListener("click",()=>{
     document.body.classList.toggle("dark")
+    let currentRotate = parseInt(getComputedStyle(sunMoonBox).getPropertyValue("--rotation"))
+    sunMoonBox.style.setProperty("--rotation", currentRotate + 180)
+    
 })
